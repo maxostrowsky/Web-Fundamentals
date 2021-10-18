75fb934a83d5616155348176cd4c59dd8b695582
maxostrowsky <max.ostrowsky@yahoo.com>
2021-10-17 22:04:09 -0700

    loop challenges

---
+++ JS/loop_challenges.js
@@ -0,0 +1,24 @@
+for (var x=1; x<20; x++;) {
+    if (x%2=1);{
+        console.log(x);
+    }
+}
+    
+for (var i=100; i>0; i-=3){
+    console.log(i);
+}
+    
+for (var y=4; y>-4; y-=1.5){
+    console.log(y)
+}
+var sum = 0 
+for (var z=1; z<101; z++){
+    sum= z + sum
+}
+console.log(sum)
+
+var zum=1
+for(var q=1; q<13; q++){
+    zum=q*zum
+}
+console.log(zum)
