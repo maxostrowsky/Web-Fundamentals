aebb8401baf1e3f7ebccc18c198be27e3dbe9962
maxostrowsky <max.ostrowsky@yahoo.com>
2021-10-18 21:02:14 -0700

    video preview

---
+++ JS/video preview/script.js
@@ -0,0 +1,11 @@
+console.log("page loaded...");
+
+function play(element){
+    element.muted===true;
+    element.play(video)
+}
+
+function pause(element){
+    element.muted===true;
+    element.pause(video)
+}
